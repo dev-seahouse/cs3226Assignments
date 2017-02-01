0cec6da198172779b4d9477d253fa94f48d436b5
VivianLow <vivian198912@gmail.com>
2017-02-01 16:52:22 +0800

    copy <head> and <script> content into template

--- rankteam1/resources/views/template.blade.php
+++ rankteam1/resources/views/template.blade.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
-    <title>Test App</title>
-    <link rel="stylesheet" href="THAT BOOTSTRAP CSS STUFFS"...>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CS3233 Ranklist 2017</title>
+    <link rel="icon" type="image/png" href="img/omega.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/r/bs-3.3.5/jq-2.1.4,dt-1.10.8/datatables.min.css" />
+    <link href="https://fonts.googleapis.com/css?family=Alegreya+Sans|Open+Sans" rel="stylesheet">
+    
+    <link rel="stylesheet" type="text/css" href="css/ourStyle.css" />
   </head>
   <body>
     @yield('main') <!-- Blade command: include section from child file -->
     @include('footer') <!-- Blade command: include other blade file -->
-    <script src="THAT BOOSTRAP JS STUFF">
+    
+    <!--need to include these 2 scripts below in order to collapse navbar-->
+    <script type="text/javascript" src="https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js">
+    </script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
+
+    <!--the 3 scripts below are dependecies for DataTables-->
+    <!--src=https://datatables.net/examples/styling/bootstrap.html-->
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
+
+    <script src="js/ourScript.js"></script>
   </body>
 </html>
