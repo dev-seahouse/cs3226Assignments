e330db218621cfd0fac307d09ca7a3109bda88c0
dev-seahouse <xinkenan.11@hotmail.com>
2017-02-09 21:51:25 +0800

    add migration to score components

---
+++ database/migrations/2017_02_09_130834_add_components_definitions_to_component_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddComponentsDefinitionsToComponentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('components', function (Blueprint $table) {
+            $score_component_types = ['MC','TC', 'HW', 'BS', 'KS', 'AC'];
+            foreach($score_component_types as $c){
+                App\Component::firstOrCreate('name'=> $c);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('components', function (Blueprint $table) {
+            //
+        });
+    }
+}
