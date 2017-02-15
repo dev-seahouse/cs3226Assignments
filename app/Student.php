323b422be285a7b73ef45742c8150d02fca5f18e
dev-seahouse <xinkenan.11@hotmail.com>
2017-02-16 04:19:45 +0800

    :mouse2: exp get record

--- app/Student.php
+++ app/Student.php
@@ -9,12 +9,33 @@ class Student extends Model
     public function comment() {
       return $this->hasOne('App\Comment');
     }
-    
+
     public function records() {
       return $this->hasMany('App\Record');
     }
-    
+
     public function scores() {
       return $this->hasMany('App\Score');
     }
+
+    public static function getRecordAndScore(){
+
+    }
+
+     // For each student, retrieve comments AND records AND scores
+    // return \App\Student::with('comment')->with('records')->with('scores')->get();
+
+    // For each student, retrieve all achievements with description
+    // return \App\Record::with(['student','achievement'])->get();
+
+    // For each achievement, get the records. Able to list which student has the highest points
+    // return \App\Achievement::with('records')->get();
+
+    // For each student, retrieve all achievements with description
+    // return \App\Record::with(['student','achievement'])->get();
+
+    // For each student, retrieve all scores
+    /*return \App\Score::with('component')
+              ->join('components', 'components.id', '=', 'scores.component_id')
+              ->get();*/
 }
