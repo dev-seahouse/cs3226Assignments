3905750890123b8bdcc607488efd515deb04b7f7
shawnlimjq <shawnlimjq@hotmail.com>
2017-02-19 22:41:21 +0800

    Fixed Http Not Found Exception
    Error page will show instead of showing laravel default

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('template') <!-- use the SAME template -->
+@section('main') <!-- also a section called main but different content -->
+<div class="container-fluid text-container">
+</div>
+@stop
