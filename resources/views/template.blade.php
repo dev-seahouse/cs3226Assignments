e8c37841c9e31d866d4e9bd1c1e7ea61cf99a5ac
shawnlimjq <shawnlimjq@hotmail.com>
2017-02-20 00:28:37 +0800

    Fixed Dropdown Problem
    Temporarily removed bootstrap.js to solve the drop down problem. Doesn't
    have an issue currently. Everything is still fully functional and
    responsive.

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -18,8 +18,8 @@
 
     <!--bootstrap must come before datatables in order to collapse navbar (conflicts)-->
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
-    </script>
+    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>-->
     <script type="text/javascript" src="https://cdn.datatables.net/r/bs-3.3.5/jqc-1.11.3,dt-1.10.8/datatables.min.js">
     </script>
     
