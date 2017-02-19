b0755319d2f3916520b901011f3d980abe2bfaae
shawnlimjq <shawnlimjq@hotmail.com>
2017-02-19 22:43:40 +0800

    Fixed error in commiting last commit
    Overwrite last bugged commit

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,6 @@
 @extends('template') <!-- use the SAME template -->
 @section('main') <!-- also a section called main but different content -->
 <div class="container-fluid text-container">
+  <a href="/"><img id="errorImage" src="img/error.png" alt="Sorry! We couldn't find that page. Try going to our home page."></a>
 </div>
 @stop
