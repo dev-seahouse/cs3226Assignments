a7f11d36f199aba7f7272e6ff2b742a1b35da978
VivianLow <vivian198912@gmail.com>
2017-02-23 00:02:35 +0800

    Display rest of achievements on achievement page

--- resources/views/acBoard.blade.php
+++ resources/views/acBoard.blade.php
@@ -2,7 +2,7 @@
 @if (count($subRecords) > 0)
 <ul class="list-group row">
   @foreach ($subRecords as $record)
-  <li class="list-group-item col-xs-6 col-sm-4 col-md-3">
+  <li class="list-group-item col-xs-6 col-sm-4 col-md-3 col-lg-2">
     <a href="{{ route('student', ['id' => $record->sId]) }}">{{ $record->name }}</a>
   </li>
   @endforeach
--- resources/views/achievement.blade.php
+++ resources/views/achievement.blade.php
@@ -4,8 +4,8 @@
   <h1 class="text-center no-margin">Achievement Board</h1>
 
   <div class="row">
-    <div class="col-sm-1"></div>
-    <div class="col-sm-10">
+    <div class="col-xs-1"></div>
+    <div class="col-xs-10">
       <?php 
       $subRecords = $records->where('aId', 1);
       $title = $achievements->where('id', 1)->first()->title;
@@ -18,7 +18,6 @@
       ?>
       @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
       
-      
       @for ($i = 1; $i <= 3; $i++)
       <?php 
       $subRecords = $records->where('aId', 3)->where('points', $i);
@@ -27,9 +26,43 @@
       @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
       @endfor
       
+      @for ($i = 1; $i <= 3; $i++)
+      <?php 
+      $subRecords = $records->where('aId', 4)->where('points', $i);
+      $title = $achievements->where('id', 4)->first()->title.' '.$i.'/3';
+      ?>
+      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
+      @endfor
+      
+      <?php 
+      $subRecords = $records->where('aId', 5);
+      $title = $achievements->where('id', 5)->first()->title;
+      ?>
+      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
+      
+      <?php 
+      $subRecords = $records->where('aId', 6);
+      $title = $achievements->where('id', 6)->first()->title;
+      ?>
+      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
+      
+      @for ($i = 1; $i <= 6; $i++)
+      <?php 
+      $subRecords = $records->where('aId', 7)->where('points', $i);
+      $title = $achievements->where('id', 7)->first()->title.' '.$i.'/6';
+      ?>
+      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
+      @endfor
+      
+      <?php 
+      $subRecords = $records->where('aId', 8);
+      $title = $achievements->where('id', 8)->first()->title;
+      ?>
+      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
+      
       </div>
     </div>
-    <div class="col-sm-1"></div>
+    <div class="col-xs-1"></div>
   </div>
 </div>
 
