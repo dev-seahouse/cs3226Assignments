8085cf31601dd48336977eecb394626511895c4e
VivianLow <vivian198912@gmail.com>
2017-02-22 14:14:40 +0800

    Edit the layout of Create page to follow Edit page

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,33 +1,46 @@
 @extends('template') <!-- use the SAME template -->
 @section('main') <!-- also a section called main but different content -->
 <div class="container-fluid">
-  <h2>CREATE STUDENT</h2>
-
+  <h2 align="center">CREATE STUDENT</h2>
   {!! Form::open(['url' => 'createStudent', 'method' => 'put', 'files' => true]) !!}
-  <!-- It is more user friendly to highlight fields with error(s) and display 
-an error message near its relevant field and you are encouraged to do so. -->
-  <div class="form-group">
-    {!! Form::label('nick', 'Nick name:', ['class' => 'control-label']) !!}
-    @include('invalidError', array('field'=>'nick'))
-    {!! Form::text('nick', null, ['class' => 'form-control']) !!}
-
-    {!! Form::label('name', 'Full name:', ['class' => 'control-label']) !!}
-    @include('invalidError', array('field'=>'name'))
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-
-    {!! Form::label('kattis', 'Kattis account:', ['class' => 'control-label']) !!}
-    @include('invalidError', array('field'=>'kattis'))
-    {!! Form::text('kattis', null, ['class' => 'form-control']) !!}
-
-    {!! Form::label('nationality', 'Nationality:', ['class' => 'control-label']) !!}
-    {!! Form::select('nationality', array('SGP' => 'SGP - Singaporean', 'CHN' => 'CHN - Chinese', 'VNM' => 'VNM - Vietnamese', 'IDN' => 'IDN - Indonesian', 'OTH' => 'Other Nationality')) !!}
-    <br>
-    {!! Form::label('profile_pic', 'Profile picture:', ['class' => 'control-label']) !!}
-    @include('invalidError', array('field'=>'profile_pic'))
-    {!! Form::file('profile_pic', null, ['class' => 'form-control']) !!}
-  </div>
-  <div class="form-group">
-    {!! Form::submit('Submit', ['class' => 'form-control btn btn-primary']) !!}
+  <div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-10">
+      <div class="container-fluid">
+        <div class="form-group">
+          {!! Form::label('nick', 'Nick name:', ['class' => 'control-label']) !!}
+          @include('invalidError', array('field'=>'nick'))
+          <div class="{{ $errors->has('nick') ? 'has-error' : '' }}">{!! Form::text('nick', null, ['class' => 'form-control']) !!}</div>
+        </div>
+        <div class="form-group">
+          {!! Form::label('name', 'Full name:', ['class' => 'control-label']) !!}
+          @include('invalidError', array('field'=>'name'))
+          <div class="{{ $errors->has('nick') ? 'has-error' : '' }}">{!! Form::text('name', null, ['class' => 'form-control']) !!}</div>
+        </div>
+        <div class="form-group">
+          {!! Form::label('kattis', 'Kattis account:', ['class' => 'control-label']) !!}
+          @include('invalidError', array('field'=>'kattis'))
+          <div class="{{ $errors->has('nick') ? 'has-error' : '' }}">{!! Form::text('kattis', null, ['class' => 'form-control']) !!}</div>
+        </div>
+        <div class="form-group">
+          {!! Form::label('nationality', 'Nationality:', ['class' => 'control-label']) !!}
+          {!! Form::select('nationality', array('SGP' => 'SGP - Singaporean', 'CHN' => 'CHN - Chinese', 'VNM' => 'VNM - Vietnamese', 'IDN' => 'IDN - Indonesian', 'OTH' => 'Other Nationality')) !!}
+        </div>
+        <div class="form-group">
+          {!! Form::label('profile_pic', 'Profile picture:', ['class' => 'control-label']) !!}
+          @include('invalidError', array('field'=>'profile_pic'))
+          {!! Form::file('profile_pic', null, ['class' => 'form-control']) !!}
+        </div><br>
+        <div class="form-group">
+          <div class="col-xs-3"></div>
+          <div class="col-xs-6">
+            {!! Form::submit('Submit', ['class' => 'form-control btn btn-primary']) !!}
+          </div>
+          <div class="col-xs-3"></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-1"></div>
   </div>
   {!! Form::close() !!}
 </div>
