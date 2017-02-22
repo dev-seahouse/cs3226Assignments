9acf436642bdd2524e94ba2536efe68323640f32
VivianLow <vivian198912@gmail.com>
2017-02-23 01:41:22 +0800

    Switch to using panels to display achievements

--- resources/views/acBoard.blade.php
+++ resources/views/acBoard.blade.php
@@ -1,13 +1,35 @@
+<?php 
+$subRecords = $records->where('aId', $id)->where('points', $point);
+$ac = $achievements->where('id', $id)->first();
+if ($ac->max_points != 1) {
+  $title = $ac->title.' '.$point.'/'.$ac->max_points;
+}
+else {
+  $title = $ac->title;
+}
+?>
 @if (count($subRecords) > 0)
 <br>
-<h3>{{ $title }}</h3>
-<div class="container-fluid board">
-  <ul class="list-group row">
-    @foreach ($subRecords as $record)
-    <li class="list-group-item board-item col-xs-6 col-sm-4 col-md-3 col-lg-2">
-      <a href="{{ route('student', ['id' => $record->sId]) }}">{{ $record->name }}</a>
-    </li>
-    @endforeach
-  </ul>
-</div>
+<section class="{{ $sectionClass }}">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="{{ $headingId }}">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#{{ $collapseId }}" aria-expanded="false" aria-controls="{{ $collapseId }}">
+          <h4><b>{{ $title }}</b></h4>
+        </a>
+      </h4>
+    </div>
+    <div id="{{ $collapseId }}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="{{ $headingId }}">
+      <div class="panel-body">
+        <ul class="list-group row">
+          @foreach ($subRecords as $record)
+          <li class="list-group-item board-item col-xs-12 col-sm-6 col-md-3">
+            <a href="{{ route('student', ['id' => $record->sId]) }}">{{ $record->name }}</a>
+          </li>
+          @endforeach
+        </ul>
+      </div>
+    </div>
+  </div>
+</section>
 @endif
--- resources/views/achievement.blade.php
+++ resources/views/achievement.blade.php
@@ -2,71 +2,32 @@
 @section('main') <!-- also a section called main but different content -->
 <div class="container-fluid">
   <h1 class="text-center no-margin">Achievement Board</h1>
-
+  <br>
   <div class="row">
-    <div class="col-xs-1"></div>
-    <div class="col-xs-10">
-      <?php 
-      $subRecords = $records->where('aId', 1);
-      $title = $achievements->where('id', 1)->first()->title;
-      ?>
-      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
-      
-      <?php 
-      $subRecords = $records->where('aId', 2);
-      $title = $achievements->where('id', 2)->first()->title;
-      ?>
-      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
-      
-      @for ($i = 1; $i <= 3; $i++)
-      <?php 
-      $subRecords = $records->where('aId', 3)->where('points', $i);
-      $title = $achievements->where('id', 3)->first()->title.' '.$i.'/3';
-      ?>
-      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
-      @endfor
-      
-      @for ($i = 1; $i <= 3; $i++)
-      <?php 
-      $subRecords = $records->where('aId', 4)->where('points', $i);
-      $title = $achievements->where('id', 4)->first()->title.' '.$i.'/3';
-      ?>
-      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
-      @endfor
-      
-      <?php 
-      $subRecords = $records->where('aId', 5);
-      $title = $achievements->where('id', 5)->first()->title;
-      ?>
-      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
-      
-      <?php 
-      $subRecords = $records->where('aId', 6);
-      $title = $achievements->where('id', 6)->first()->title;
-      ?>
-      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
-      
-      @for ($i = 1; $i <= 6; $i++)
-      <?php 
-      $subRecords = $records->where('aId', 7)->where('points', $i);
-      $title = $achievements->where('id', 7)->first()->title.' '.$i.'/6';
-      ?>
-      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
-      @endfor
-      
-      <?php 
-      $subRecords = $records->where('aId', 8);
-      $title = $achievements->where('id', 8)->first()->title;
-      ?>
-      @include('acBoard', array('title' => $title, 'subRecords' => $subRecords))
-      
-      </div>
+    <div class="col-xs-1 col-sm-1 col-md-2"></div>
+    <div class="col-xs-10 col-sm-10 col-md-8">
+      <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+        @include('acBoard', array('id' => 1, 'point' => 1, 'sectionClass' => 'ac1', 'headingId' => 'heading1', 'collapseId' => 'collapse1'))
+        @include('acBoard', array('id' => 2, 'point' => 1, 'sectionClass' => 'ac2', 'headingId' => 'heading2', 'collapseId' => 'collapse2'))
+        @include('acBoard', array('id' => 3, 'point' => 1, 'sectionClass' => 'ac3a', 'headingId' => 'heading3', 'collapseId' => 'collapse3'))
+        @include('acBoard', array('id' => 3, 'point' => 2, 'sectionClass' => 'ac3b', 'headingId' => 'heading4', 'collapseId' => 'collapse4'))
+        @include('acBoard', array('id' => 3, 'point' => 3, 'sectionClass' => 'ac3c', 'headingId' => 'heading5', 'collapseId' => 'collapse5'))
+        @include('acBoard', array('id' => 4, 'point' => 1, 'sectionClass' => 'ac4a', 'headingId' => 'heading6', 'collapseId' => 'collapse6'))
+        @include('acBoard', array('id' => 4, 'point' => 2, 'sectionClass' => 'ac4b', 'headingId' => 'heading7', 'collapseId' => 'collapse7'))
+        @include('acBoard', array('id' => 4, 'point' => 3, 'sectionClass' => 'ac4c', 'headingId' => 'heading8', 'collapseId' => 'collapse8'))
+        @include('acBoard', array('id' => 5, 'point' => 1, 'sectionClass' => 'ac5', 'headingId' => 'heading9', 'collapseId' => 'collapse9'))
+        @include('acBoard', array('id' => 6, 'point' => 1, 'sectionClass' => 'ac6', 'headingId' => 'heading10', 'collapseId' => 'collapse10'))
+        @include('acBoard', array('id' => 7, 'point' => 1, 'sectionClass' => 'ac7a', 'headingId' => 'heading11', 'collapseId' => 'collapse11'))
+        @include('acBoard', array('id' => 7, 'point' => 2, 'sectionClass' => 'ac7b', 'headingId' => 'heading12', 'collapseId' => 'collapse12'))
+        @include('acBoard', array('id' => 7, 'point' => 3, 'sectionClass' => 'ac7c', 'headingId' => 'heading13', 'collapseId' => 'collapse13'))
+        @include('acBoard', array('id' => 7, 'point' => 4, 'sectionClass' => 'ac7d', 'headingId' => 'heading14', 'collapseId' => 'collapse14'))
+        @include('acBoard', array('id' => 7, 'point' => 5, 'sectionClass' => 'ac7e', 'headingId' => 'heading15', 'collapseId' => 'collapse15'))
+        @include('acBoard', array('id' => 7, 'point' => 6, 'sectionClass' => 'ac7f', 'headingId' => 'heading16', 'collapseId' => 'collapse16'))
+        @include('acBoard', array('id' => 8, 'point' => 1, 'sectionClass' => 'ac8', 'headingId' => 'heading17', 'collapseId' => 'collapse17'))
+      </div> <!-- end of multi panel -->
     </div>
     <div class="col-xs-1"></div>
   </div>
 </div>
-
-
-
-
+<br>
 @stop
