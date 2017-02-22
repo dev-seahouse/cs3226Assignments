6f2915117349236c7fce0bc957b7d60eb9344413
VivianLow <vivian198912@gmail.com>
2017-02-23 00:09:06 +0800

    Hide achievement if no students have it

--- resources/views/acBoard.blade.php
+++ resources/views/acBoard.blade.php
@@ -1,5 +1,5 @@
-<h3>{{ $title }}</h3>
 @if (count($subRecords) > 0)
+<h3>{{ $title }}</h3>
 <ul class="list-group row">
   @foreach ($subRecords as $record)
   <li class="list-group-item col-xs-6 col-sm-4 col-md-3 col-lg-2">
@@ -7,6 +7,4 @@
   </li>
   @endforeach
 </ul>
-@else
-<p>There are no students with this achievement.</p>
 @endif
