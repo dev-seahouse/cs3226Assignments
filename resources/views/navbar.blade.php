213852ea5f870d590865051bd7ec55508d28dd4c
nuslarry <e0147977@u.nus.edu>
2017-02-23 18:21:14 +0800

    add link to navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -36,8 +36,71 @@ $uri = $_SERVER['REQUEST_URI'];
         }
         ?>
 		@if (Auth::guest())
-		 @else
+		@else
         <li><a href="{{route('create')}}">Create New Student</a></li>
+        <li class="dropdown" >
+            <a href="{{route('create')}}" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" >
+              Edit Students' data <span class="caret"></span>
+            </a>
+        
+
+            <ul class="dropdown-menu" role="menu">
+<!--          <li>
+                <a href="{{ route('logout') }}">
+                  Logout
+                </a>
+              </li> -->
+              <li class="dropdown-submenu">
+                <a class="test" >MC <b class="right-caret"></b></a>
+                <ul class="dropdown-menu" style="width:200px;float:left;left:160px;top:0px;">
+                  @for ($i = 1; $i <= 9; $i++)
+                    <li><a href="{{URL::route('editSection', array('section' => "MC".$i))}}">MC{{$i}}</a></li>
+                  @endfor
+                </ul>
+              </li>
+                <li class="dropdown-submenu">
+                <a class="test" >TC <b class="right-caret"></b></a>
+                <ul class="dropdown-menu" style="width:200px;float:left;left:160px;top:25px;">
+                  @for ($i = 1; $i <= 2; $i++)
+                    <li><a href="{{URL::route('editSection', array('section' => "TC".$i))}}">TC{{$i}}</a></li>
+                  @endfor
+                </ul>
+              </li>
+              </li>
+              <li class="dropdown-submenu">
+                <a class="test" >HW <b class="right-caret"></b></a>
+                <ul class="dropdown-menu" style="width:200px;float:left;left:160px;top:50px;">
+                  @for ($i = 1; $i <= 10; $i++)
+                    <li><a href="{{URL::route('editSection', array('section' => "HW".$i))}}">HW{{$i}}</a></li>
+                  @endfor
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a class="test" >BS <b class="right-caret"></b></a>
+                <ul class="dropdown-menu" style="width:200px;float:left;left:160px;top:75px;">
+                  @for ($i = 1; $i <= 9; $i++)
+                    <li><a href="{{URL::route('editSection', array('section' => "BS".$i))}}">BS{{$i}}</a></li>
+                  @endfor
+                </ul>
+              </li>            
+              <li class="dropdown-submenu">
+                <a class="test" >KS <b class="right-caret"></b></a>
+                <ul class="dropdown-menu" style="width:200px;float:left;left:160px;top:75px;">
+                  @for ($i = 1; $i <= 12; $i++)
+                    <li><a href="{{URL::route('editSection', array('section' => "KS".$i))}}">KS{{$i}}</a></li>
+                  @endfor
+                </ul>
+              </li>   
+              <li class="dropdown-submenu">
+                <a class="test" >AC <b class="right-caret"></b></a>
+                <ul class="dropdown-menu" style="width:200px;float:left;left:160px;top:75px;">
+                  @for ($i = 1; $i <= 8; $i++)
+                    <li><a href="{{URL::route('editSection', array('section' => "AC".$i))}}">AC{{$i}}</a></li>
+                  @endfor
+                </ul>
+              </li>   
+            </ul>
+
 		@endif
         <li><a href="{{route('achievement')}}">Achievement</a></li>
         <li><a href="{{route('help')}}">Help</a></li>
@@ -72,3 +135,22 @@ $uri = $_SERVER['REQUEST_URI'];
     </div>
   </div>
 </nav>
+<script>
+// $(document).ready(function(){
+//   $('.dropdown-submenu a.test').on("click", function(e){
+//     $(this).next('ul').toggle();
+//     e.stopPropagation();
+//     e.preventDefault();
+//   });
+// });
+var previous=null;
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    if(previous!=null)previous.toggle();
+    previous=$(this).next('ul');
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
+</script> 。
