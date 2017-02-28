0bcb6139af7bf415d829a033fa07d7654b1a7e8e
Bevin Seetoh Jia Jin <a0134234@u.nus.edu>
2017-02-28 21:59:44 +0800

    Add admin and two student account into seed

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -71,34 +71,34 @@ class UserSeeder extends Seeder
           'updated_at' => date('Y-m-d H:m:s')
         ));
       
-//        DB::table('users')->insert(
-//          array( 
-//            'name' => 'admin',
-//            'email' => '', 
-//            'password' => 'qwerty123', 
-//            'lang_pref' => 'en', 
-//            'role' => 'admin',
-//            'student_id' => null
-//          ));
-        
-//        DB::table('users')->insert(
-//          array( 
-//            'name' => '',
-//            'email' => '', 
-//            'password' => '', 
-//            'lang_pref' => 'en', 
-//            'role' => 'student',
-//            'student_id' => null //some id
-//          ));
-//      
-//        DB::table('users')->insert(
-//          array( 
-//            'name' => '',
-//            'email' => '', 
-//            'password' => '', 
-//            'lang_pref' => 'zh', 
-//            'role' => 'student',
-//            'student_id' => null //some id
-//          ));
+      DB::table('users')->insert(
+        array( 
+          'name' => 'admin',
+          'email' => 'tanmunaw@u.nus.edu', 
+          'password' => bcrypt('qwerty123'), 
+          'lang_pref' => 'en', 
+          'role' => 'admin',
+          'student_id' => null
+        ));
+
+      DB::table('users')->insert(
+        array( 
+          'name' => 'Husky',
+          'email' => 'student1@officialranklist.tk', 
+          'password' => bcrypt('qwerty123'), 
+          'lang_pref' => 'en', 
+          'role' => 'student',
+          'student_id' => 13 //some id
+        ));
+
+      DB::table('users')->insert(
+        array( 
+          'name' => 'Chopper',
+          'email' => 'student2@officialranklist.tk', 
+          'password' => bcrypt('qwerty123'), 
+          'lang_pref' => 'zh', 
+          'role' => 'student',
+          'student_id' => 39 //some id
+        ));
     }
 }
