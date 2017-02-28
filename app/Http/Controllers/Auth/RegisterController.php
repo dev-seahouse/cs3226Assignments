9c9c5436f4d209ee05ac8e37318a9a2776eec982
Bevin Seetoh Jia Jin <a0134234@u.nus.edu>
2017-02-28 20:57:43 +0800

    Hide /register route, modify htaccess redirect http to https (uncomment lines on server), add analytics code to template

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,4 +68,15 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
         ]);
     }
+    
+    public function showRegistrationForm()
+    {
+        return redirect('login');
+    }
+
+    public function register()
+    {
+
+    }
+
 }
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -22,6 +22,15 @@
     <!--the 3 scripts below are dependecies for DataTables-->
     <!--src=https://datatables.net/examples/styling/bootstrap.html-->
     <script src="{{ asset('js/app.js') }}"></script>
+    
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+      ga('create', 'UA-92815505-1', 'auto');
+      ga('send', 'pageview');
+    </script>
   </body>
 </html>
