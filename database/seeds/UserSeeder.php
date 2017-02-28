69e5b53726254c5e6423ecdcdd93fcce19bec74f
Bevin Seetoh Jia Jin <a0134234@u.nus.edu>
2017-02-28 21:49:23 +0800

    Added seeds for admins(us) all passwords are qwerty123

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -11,34 +11,94 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(
-          array( 
-            'name' => 'admin',
-            'email' => '', 
-            'password' => 'qwerty123', 
-            'lang_pref' => 'en', 
-            'role' => 'admin',
-            'student_id' => null
-          ));
-        
-        DB::table('users')->insert(
-          array( 
-            'name' => '',
-            'email' => '', 
-            'password' => '', 
-            'lang_pref' => 'en', 
-            'role' => 'student',
-            'student_id' => null //some id
-          ));
+      DB::table('users')->insert(
+        array( 
+          'name' => 'bevin',
+          'email' => 'bevinseetoh@hotmail.com', 
+          'password' => bcrypt('qwerty123'), 
+          'lang_pref' => 'en', 
+          'role' => 'admin',
+          'student_id' => null,
+          'created_at' => date('Y-m-d H:m:s'),
+          'updated_at' => date('Y-m-d H:m:s')
+        ));
       
-        DB::table('users')->insert(
-          array( 
-            'name' => '',
-            'email' => '', 
-            'password' => '', 
-            'lang_pref' => 'zh', 
-            'role' => 'student',
-            'student_id' => null //some id
-          ));
+      DB::table('users')->insert(
+        array( 
+          'name' => 'kenan',
+          'email' => 'sayhi.kenan@gmail.com', 
+          'password' => bcrypt('qwerty123'), 
+          'lang_pref' => 'en', 
+          'role' => 'admin',
+          'student_id' => null,
+          'created_at' => date('Y-m-d H:m:s'),
+          'updated_at' => date('Y-m-d H:m:s')
+        ));
+      
+      DB::table('users')->insert(
+        array( 
+          'name' => 'vivian',
+          'email' => 'vivian198912@gmail.com', 
+          'password' => bcrypt('qwerty123'), 
+          'lang_pref' => 'en', 
+          'role' => 'admin',
+          'student_id' => null,
+          'created_at' => date('Y-m-d H:m:s'),
+          'updated_at' => date('Y-m-d H:m:s')
+        ));
+      
+      DB::table('users')->insert(
+        array( 
+          'name' => 'larry',
+          'email' => 'larry1285@gmail.com', 
+          'password' => bcrypt('qwerty123'), 
+          'lang_pref' => 'en', 
+          'role' => 'admin',
+          'student_id' => null,
+          'created_at' => date('Y-m-d H:m:s'),
+          'updated_at' => date('Y-m-d H:m:s')
+        ));
+      
+      DB::table('users')->insert(
+        array( 
+          'name' => 'shawn',
+          'email' => 'shawnlimjq@hotmail.com', 
+          'password' => bcrypt('qwerty123'), 
+          'lang_pref' => 'en', 
+          'role' => 'admin',
+          'student_id' => null,
+          'created_at' => date('Y-m-d H:m:s'),
+          'updated_at' => date('Y-m-d H:m:s')
+        ));
+      
+//        DB::table('users')->insert(
+//          array( 
+//            'name' => 'admin',
+//            'email' => '', 
+//            'password' => 'qwerty123', 
+//            'lang_pref' => 'en', 
+//            'role' => 'admin',
+//            'student_id' => null
+//          ));
+        
+//        DB::table('users')->insert(
+//          array( 
+//            'name' => '',
+//            'email' => '', 
+//            'password' => '', 
+//            'lang_pref' => 'en', 
+//            'role' => 'student',
+//            'student_id' => null //some id
+//          ));
+//      
+//        DB::table('users')->insert(
+//          array( 
+//            'name' => '',
+//            'email' => '', 
+//            'password' => '', 
+//            'lang_pref' => 'zh', 
+//            'role' => 'student',
+//            'student_id' => null //some id
+//          ));
     }
 }
