b04787ffe30fcb6711d6560a1f02fc711f766f3e
shawnlimjq <shawnlimjq@hotmail.com>
2017-03-03 19:38:05 +0800

    Extracted css from index page
    Extracted css from index page to app.scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,10 +19,10 @@
   @endif
   
   <h1 class="text-center no-margin">Rankings</h1>
-  <div class="text-center no-margin" style="padding-top:10px">
+  <div class="text-center no-margin sharing">
  <div class="fb-like" data-href="http://cs3226officialranklist.tk/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
    </div>
-   <div class="text-center no-margin" style="padding-top:10px">
+   <div class="text-center no-margin sharing">
     <a href="https://twitter.com/CS3226_Official" class="twitter-follow-button" data-size="normal" style="vertical-align: text-bottom;">Follow @CS3226_Official</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
   </div>
   <h5 class="text-center">Last updated at {{ $last_updated }}</h5>
