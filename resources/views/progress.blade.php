3eac9cbe3d84580c4410a96deb81b159d9a3ca32
nuslarry <e0147977@u.nus.edu>
2017-03-06 02:08:50 +0800

    progress page localization

--- resources/views/progress.blade.php
+++ resources/views/progress.blade.php
@@ -1,12 +1,12 @@
 @extends('template') <!-- use the SAME template -->
 @section('main') <!-- also a section called main but different content -->
 <div class="container-fluid">
-  <h1 class="text-center no-margin">Progress Chart</h1>
+  <h1 class="text-center no-margin">{{__('messages.progressChart')}}</h1>
   <div class="row hidden-sm">
     <div class="col-sm-1"></div>
     <div class="col-sm-10 text-center">
       <div>
-        <button id="progressChartHideButton" class="btn btn-primary btn-fixed-width" style="margin: 1em 0em 1em 0em;">Toggle all students</button>
+        <button id="progressChartHideButton" class="btn btn-primary btn-fixed-width" style="margin: 1em 0em 1em 0em;">{{__('messages.ToggleAllStudent')}}</button>
         <canvas id="progressChart"></canvas>
       </div>
     </div>
