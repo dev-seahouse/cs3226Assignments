61606f8032203840ab148a4a11dd2d6264e59208
nuslarry <e0147977@u.nus.edu>
2017-03-06 02:08:32 +0800

    message page localization

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -10,7 +10,7 @@
     </div>
   @endif
   
-  <h1 class="text-center no-margin">Message Board</h1>
+  <h1 class="text-center no-margin">{{__('messages.messageBoard')}}</h1>
   <br>
   <div class="row">
     @if (Auth::user()->role == 'student')
