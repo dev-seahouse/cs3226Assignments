2af98a8f2fc69f83f135ad3d4d1a3b14050442e2
nuslarry <e0147977@u.nus.edu>
2017-03-06 02:08:01 +0800

    en message update

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,29 +2,90 @@
    return [
 
       'submit' => 'submit',
-
+      'update' => 'update',
+      'edit' => '編輯',
+      'delete' => '刪除',
+      //navbar
       'mainTitle' => 'CS3233 Ranklist 2017',
-      'editStudentData'=> 'edit Student\'s Data',
+      'editMode'=> 'Edit Mode',
+      'language'=> 'language',
+      'editStudentData'=> "edit Student's Data",
       'messages' => 'Messages',
-      'achievements' => 'Achievements',
-      'progressChart' => 'Progress Chart',
+
       'editComponent' => 'edit Component',
       'help' => 'Help',
       'login' => 'login',
       'register' => 'register',
       'language'=> 'language',
 
-      'createNewStudent'=>'Create New Student',
-
+      //edit student data
+      'editComponent'=> 'edit Component',
       'rankings' => 'Rankings',
       'updateDesc' => 'Last updated at ',
 
+      //create new student
+      'createNewStudent'=>'Create New Student',
+      'createStudent'=>'Create Student',
       'nickName' => 'nick Name ',
       'fullName' => 'Full name ',
-      'kattisAccount' => 'kattos Account ',
+      'kattisAccount' => 'kattis Account ',
       'nationality' => 'nationality ',
       'uploadPicDesc' => 'Profile picture: (Click save to confirm)',
-
+      //messages
       'messageBoard' => 'Message Board',
+      //achievement
+      'achievementBoard' => 'Achievement Board',
+      'let-it-begin'=>'let it begin',
+      'Quick-starter'=>'Quick starter',
+      'Active-in-class-1/3'=>'Active in class 1/3',
+      'Active-in-class-2/3'=>'Active in class 1/3',
+      'Active-in-class-3/3'=>'Active in class 1/3',
+      'Surprise-us-1/3'=>'Active in class 1/3',
+      'Surprise-us-2/3'=>'Active in class 2/3',
+      'Surprise-us-3/3'=>'Active in class 3/3',
+      'High-determination'=>'High determination',
+      'Bookworm'=>'Bookworm',
+      'Kattis-apprentice-1/6'=>'Kattis apprentice 1/6',
+      'Kattis-apprentice-2/6'=>'Kattis apprentice 2/6',
+      'Kattis-apprentice-3/6'=>'Kattis apprentice 3/6',
+      'Kattis-apprentice-4/6'=>'Kattis apprentice 4/6',
+      'Kattis-apprentice-5/6'=>'Kattis apprentice 5/6',
+      'Kattis-apprentice-6/6'=>'Kattis apprentice 6/6',
+      'CodeForces-Specialist'=>'CodeForces Specialist',
+
+      //progress
+      'progressChart' => 'Progress Chart',
+      'ToggleAllStudent' => 'Toggle All Student',
+
+      //help 
+      'ScoringScheme' => 'Scoring Scheme',
+      'Overview' => 'Overview',
+      'MC_desc' => 'MC = Weekly Mini Contest (36%)',
+      'TC_desc' => 'Two Team Contests (24%)',
+      'HW_desc' => 'Weekly Homework (15%)',
+      'PB_desc' => 'Problem Bs (9%)',
+      'KS_desc' => 'Kattis Set (12%)',
+      'AC_desc' => 'Achievement System of CS3233 (17%)',
+
+       //detail
+      'STUDENT-DETAILS' => 'STUDENT DETAILS',  
+      'student_time_detail'=>'in CS3233 S2 AY 2016/2017',
+      'SPE'=>'SPE',
+      'DIL'=>'DIL',
+      'ed'=>'ed',
+      'igence'=>'igence',
+      'component'=>'component',
+      'SUM'=>'SUM',
+      'Progress-comparison'=>'Progress comparison',
+      'Detailed-scores'=>'Detailed scores',
+      'Mini-Contests'=>'Mini Contests',
+      'Team-Contests'=>'Team Contests',
+      'Homework'=>'Homework',
+      'Problem-Bs'=>'Problem Bs',
+      'Kattis-Sets'=>'Kattis Sets',
+      'Achievements'=>'Achievements', 
+      'details'=>'details', 
+      'student-specific-comment'=>'Specific comments about this student',
+
    ];
 ?>
