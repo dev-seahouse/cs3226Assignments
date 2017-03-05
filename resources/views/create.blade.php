b836320ec78d2914130468880546f03f62f13c0d
nuslarry <e0147977@u.nus.edu>
2017-03-06 02:09:33 +0800

    create page localization

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,7 +4,7 @@
 <script src="{{ asset('js/fabric.js') }}"></script>
 <script src="{{ asset('js/darkroom.js') }}"></script>
 <div class="container-fluid"> 
-  <h2 align="center">CREATE STUDENT</h2>
+  <h2 align="center">{{__('messages.createStudent')}}</h2>
   {!! Form::open(['url' => 'createStudent', 'method' => 'put', 'files' => true]) !!}
   {{ csrf_field() }}
   <div class="row">
@@ -12,27 +12,27 @@
     <div class="col-sm-10">
       <div class="container-fluid">
         <div class="form-group">
-          {!! Form::label('nick', 'Nick name:', ['class' => 'control-label']) !!}
+          {!! Form::label('nick', __('messages.nickName'), ['class' => 'control-label']) !!}
           @include('invalidError', array('field'=>'nick'))
           <div class="{{ $errors->has('nick') ? 'has-error' : '' }}">{!! Form::text('nick', null, ['class' => 'form-control']) !!}</div>
         </div>
         <div class="form-group">
-          {!! Form::label('name', 'Full name:', ['class' => 'control-label']) !!}
+          {!! Form::label('name', __('messages.fullName'), ['class' => 'control-label']) !!}
           @include('invalidError', array('field'=>'name'))
           <div class="{{ $errors->has('nick') ? 'has-error' : '' }}">{!! Form::text('name', null, ['class' => 'form-control']) !!}</div>
         </div>
         <div class="form-group">
-          {!! Form::label('kattis', 'Kattis account:', ['class' => 'control-label']) !!}
+          {!! Form::label('kattis', __('messages.kattisAccount'), ['class' => 'control-label']) !!}
           @include('invalidError', array('field'=>'kattis'))
           <div class="{{ $errors->has('nick') ? 'has-error' : '' }}">{!! Form::text('kattis', null, ['class' => 'form-control']) !!}</div>
         </div>
         <div class="form-group">
-          {!! Form::label('nationality', 'Nationality:', ['class' => 'control-label']) !!}
+          {!! Form::label('nationality', __('messages.nationality'), ['class' => 'control-label']) !!}
           {!! Form::select('nationality', array('SGP' => 'SGP - Singaporean', 'CHN' => 'CHN - Chinese', 'VNM' => 'VNM - Vietnamese', 'IDN' => 'IDN - Indonesian', 'JPN' => 'JPN - Japanese', 'AUS' => 'AUS - Australian', 'GER' => 'GER - German', 'OTH' => 'Other Nationality')) !!}
         </div>
         <div class="form-group">
           <!--{!! Form::label('profile_pic', 'Profile picture:', ['class' => 'control-label']) !!}-->
-		    <label for="profile_pic" class="control-label">Profile picture: (Click save to confirm)</label>
+		    <label for="profile_pic" class="control-label">{{__('messages.uploadPicDesc')}}</label>
           @include('invalidError', array('field'=>'profile_pic'))
           @include('invalidError', array('field'=>'fileURL'))
 		    <input name="profile_pic" type="file" id="profile_pic" onchange="readURL(this)"><br><br><br>
@@ -43,7 +43,7 @@
         
         </div><br>
         <div class="form-group">
-          {!! Form::submit('Submit', ['class' => 'form-control btn btn-primary btn-fixed-width center-block']) !!}
+          {!! Form::submit(__('messages.submit'), ['class' => 'form-control btn btn-primary btn-fixed-width center-block']) !!}
         </div>
       </div>
     </div>
